3a2363dcc85fd8af0994c20acc09dbd14629cd02
Gregoire Peltier <pg501842@etu.unice.fr>
2018-11-06 09:58:52 +0100

    #2 implemented order and tests

--- src/main/java/recipe/Recipe.java
+++ src/main/java/recipe/Recipe.java
@@ -9,5 +9,18 @@ public class Recipe {
     Collection<Topping> toppings;
     Mix mix;
     Cooking cooking;
+    float price;
 
+    public Recipe(Dough dough, Collection<Flavor> flavors, Collection<Topping> toppings, Mix mix, Cooking cooking, float price) {
+        this.dough = dough;
+        this.flavors = flavors;
+        this.toppings = toppings;
+        this.mix = mix;
+        this.cooking = cooking;
+        this.price = price;
+    }
+
+    public float getPrice() {
+        return price;
+    }
 }

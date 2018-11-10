1473bbf535e8cf65c4efeb7b648e5ede30526448
Marion Marguerettaz <marion.marguerettaz@etu.unice.fr>
2018-11-10 15:44:11 +0100

    #19 loyalty p counts cookies

--- src/main/java/main/Customer.java
+++ src/main/java/main/Customer.java
@@ -12,6 +12,8 @@ public class Customer extends Guest {
     private int phoneNumber;
     private int email;
     private boolean loyaltyProgram = false;
+    private int cookieCount=0;
+    private boolean discount = false;
 
     public Customer(Collection<Order> orderHistory, int firstName, int lastName, int phoneNumber, int email) {
         super();
@@ -23,14 +25,43 @@ public class Customer extends Guest {
     }
 
     /**
-     * @param order
+     * Add the new order to the history of the customer
+     * For each new order add the number of coocki in the
+     * loyalty program
+     * @param order by the customer
      */
     private void addToOrderHistory(Order order) {
+        if(loyaltyProgram){
+            order.getOrderLines().forEach(orderLine ->
+            {
+                fideltyPoints(orderLine.getAmount());
+            });
+        }
         orderHistory.add(order);
     }
 
+    /**
+     * Add the amount of cookie to the cookie counter
+     * When the cookie counter is more than 30, then
+     * the customer can have a discount
+     * @param amount of cookie
+     */
+    private void fideltyPoints(int amount) {
+        if (cookieCount < 30 && !discount) {
+            cookieCount += amount;
+        } else {
+            discount = true;
+        }
+    }
+
+    /**
+     * Add the customer to the loyalty program
+     */
     public void addToLoyaltyP(){
         loyaltyProgram = true;
     }
 
+    public boolean canHaveDiscount() {
+        return discount;
+    }
 }

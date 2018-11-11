48ae3afcbf6f4e6ee75c7e360adc6c13cf142879
Francois Dore <francois.dore@etu.unice.fr>
2018-11-11 14:30:06 +0100

    Implementation Création compte

--- src/main/java/main/Customer.java
+++ src/main/java/main/Customer.java
@@ -11,13 +11,15 @@ public class Customer extends Guest {
     private String lastName;
     private String phoneNumber;
     private String email;
+    private String password;
 
-    public Customer(Collection<Order> orderHistory, String firstName, String lastName, String phoneNumber, String email) {
+    public Customer(Collection<Order> orderHistory, String firstName, String lastName, String phoneNumber, String email, String password) {
         super(email);
         this.orderHistory = orderHistory;
         this.firstName = firstName;
         this.lastName = lastName;
         this.phoneNumber = phoneNumber;
+        this.password = password;
     }
 
     /**
@@ -27,4 +29,18 @@ public class Customer extends Guest {
         orderHistory.add(order);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Customer customer = (Customer) o;
+
+        if (orderHistory != null ? !orderHistory.equals(customer.orderHistory) : customer.orderHistory != null)
+            return false;
+        if (lastName != null ? !lastName.equals(customer.lastName) : customer.lastName != null) return false;
+        if (firstName != null ? !firstName.equals(customer.firstName) : customer.firstName != null) return false;
+        if (password != null ? !password.equals(customer.password) : customer.password != null) return false;
+        return email != null ? customer.equals(customer.email) : customer.email == null;
+    }
 }

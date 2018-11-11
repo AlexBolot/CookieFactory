743c4aa9f23316eca3ba2d34c1c1abd2688e22b0
Alexandre Bolot <alexandre.bolot@etu.unice.fr>
2018-11-11 03:31:26 +0100

    #11 Adding Gherking testing of feature "Changer horaires du magasin"

--- src/test/java/StepDefinitions/ChangeScheduleStepDefs.java
+++ src/test/java/StepDefinitions/ChangeScheduleStepDefs.java
@@ -1,33 +1,152 @@
 package StepDefinitions;
 
-import cucumber.api.PendingException;
-import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import main.Day;
+import main.Manager;
+import main.Store;
+import org.junit.Assert;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.Map;
 
 public class ChangeScheduleStepDefs {
-    @Given("^The \"([^\"]*)\" is connected for a \"([^\"]*)\"$")
-    public void theIsConnectedForA(String arg0, String arg1) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+
+    private Map<String, Manager> managers = new HashMap<>();
+    private Map<String, Store> stores = new HashMap<>();
+
+    private Exception openingTimeException;
+    private Exception closingTimeException;
+
+    private Day dayFromName(String dayName) {
+        for (Day day : Day.values()) {
+            if (day.name().equalsIgnoreCase(dayName)) return day;
+        }
+        return null;
     }
 
-    @And("^The \"([^\"]*)\" consult the parameters$")
-    public void theConsultTheParameters(String arg0) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @Given("^A store \"([^\"]*)\"$")
+    public void aStore(String storeName) {
+        stores.put(storeName, new Store(15));
     }
 
-    @When("^The \"([^\"]*)\" select a new \"([^\"]*)\" and a new \"([^\"]*)\"$")
-    public void theSelectANewAndANew(String arg0, String arg1, String arg2) throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @Given("^\"([^\"]*)\" the manager of \"([^\"]*)\"$")
+    public void theManagerOf(String managerName, String storeName) {
+        Manager manager = new Manager(stores.get(storeName));
+        managers.put(managerName, manager);
     }
 
-    @Then("^The store's schedule is changed$")
-    public void theStoreSScheduleIsChanged() throws Throwable {
-        // Write code here that turns the phrase above into concrete actions
-        throw new PendingException();
+    @Given("^The \"([^\"]*)\" opens \"([^\"]*)\" at \"([^\"]*)\" and closes at \"([^\"]*)\"$")
+    public void theStoreOpensAtAndClosesAt(String storeName, String dayName, String openingTime, String closingTime) {
+
+        //Given the template Hours:Minutes
+        int opHour = Integer.parseInt(openingTime.split(":")[0]);
+        int opMinutes = Integer.parseInt(openingTime.split(":")[1]);
+
+        int clHour = Integer.parseInt(closingTime.split(":")[0]);
+        int clMinutes = Integer.parseInt(closingTime.split(":")[1]);
+
+        Day day = dayFromName(dayName);
+
+        LocalDateTime opTime = LocalDateTime.of(LocalDate.now(), LocalTime.of(opHour, opMinutes));
+        LocalDateTime clTime = LocalDateTime.of(LocalDate.now(), LocalTime.of(clHour, clMinutes));
+
+        Store store = stores.get(storeName);
+
+        store.setOpeningTime(day, opTime);
+        store.setClosingTimes(day, clTime);
+    }
+
+    @When("^\"([^\"]*)\" changes opening time of \"([^\"]*)\" to \"([^\"]*)\"$")
+    public void theManagerChangesOpeningTime(String managerName, String dayName, String openingTime) {
+
+        //Given the template Hours:Minutes
+        int opHour = Integer.parseInt(openingTime.split(":")[0]);
+        int opMinutes = Integer.parseInt(openingTime.split(":")[1]);
+
+        Day day = dayFromName(dayName);
+
+        LocalDateTime opTime = LocalDateTime.of(LocalDate.now(), LocalTime.of(opHour, opMinutes));
+
+        Manager manager = managers.get(managerName);
+
+        try {
+            manager.changeOpeningTime(day, opTime);
+
+            if (opTime.isAfter(manager.getStore().closingTime(day)))
+                Assert.fail("Expecting IllegalArgumentException to be thrown");
+
+        } catch (IllegalArgumentException iae) {
+            openingTimeException = iae;
+        }
+    }
+
+    @Then("^The \"([^\"]*)\" opening of \"([^\"]*)\" is \"([^\"]*)\"$")
+    public void theStoreOpeningIs(String storeName, String dayName, String openingTime) {
+
+        //Given the template Hours:Minutes
+        int opHour = Integer.parseInt(openingTime.split(":")[0]);
+        int opMinutes = Integer.parseInt(openingTime.split(":")[1]);
+
+        Day day = dayFromName(dayName);
+
+        LocalDateTime expectedOpTime = LocalDateTime.of(LocalDate.now(), LocalTime.of(opHour, opMinutes));
+
+        Assert.assertEquals(expectedOpTime, stores.get(storeName).openingTime(day));
+    }
+
+    @When("^\"([^\"]*)\" changes closing time of \"([^\"]*)\" to \"([^\"]*)\"$")
+    public void theManagerChangesClosingTime(String managerName, String dayName, String closingTime) {
+        //Given the template Hours:Minutes
+        int opHour = Integer.parseInt(closingTime.split(":")[0]);
+        int opMinutes = Integer.parseInt(closingTime.split(":")[1]);
+
+        Day day = dayFromName(dayName);
+
+        LocalDateTime clTime = LocalDateTime.of(LocalDate.now(), LocalTime.of(opHour, opMinutes));
+
+        Manager manager = managers.get(managerName);
+
+        try {
+            manager.changeClosingTime(day, clTime);
+
+            if (clTime.isBefore(manager.getStore().openingTime(day)))
+                Assert.fail("Expecting IllegalArgumentException to be thrown");
+
+        } catch (IllegalArgumentException iae) {
+            closingTimeException = iae;
+        }
+    }
+
+    @Then("^The \"([^\"]*)\" closing of \"([^\"]*)\" is \"([^\"]*)\"$")
+    public void theStoreClosingIs(String storeName, String dayName, String closingTime) {
+
+        //Given the template Hours:Minutes
+        int clHour = Integer.parseInt(closingTime.split(":")[0]);
+        int clMinutes = Integer.parseInt(closingTime.split(":")[1]);
+
+        Day day = dayFromName(dayName);
+
+        LocalDateTime expectedClTime = LocalDateTime.of(LocalDate.now(), LocalTime.of(clHour, clMinutes));
+
+        Assert.assertEquals(expectedClTime, stores.get(storeName).closingTime(day));
+    }
+
+    @Then("^Changing opening time of \"([^\"]*)\" fails$")
+    public void changingOpeningTimeFails(String dayName) {
+        Exception exception = new IllegalArgumentException("Trying to set opening time after closing time for " + dayFromName(dayName));
+        Assert.assertEquals(openingTimeException.getMessage(), exception.getMessage());
+        openingTimeException = null;
+    }
+
+    @Then("^Changing closing time of \"([^\"]*)\" fails$")
+    public void changingClosingTimeFails(String dayName) {
+        Exception exception = new IllegalArgumentException("Trying to set closing time before opening time for " + dayFromName(dayName));
+        Assert.assertEquals(closingTimeException.getMessage(), exception.getMessage());
+        closingTimeException = null;
     }
 }
--- src/test/resources/features/ChangeSchedule.feature
+++ src/test/resources/features/ChangeSchedule.feature
@@ -1,7 +1,25 @@
 Feature: Change store schedule
 
-  Scenario : Change store schedule
-    Given The "manager" is connected for a "store"
-    And The "manager" consult the parameters
-    When The "manager" select a new "day" and a new "timespan"
-    Then The store's schedule is changed
+  Background:
+    Given A store "store"
+    Given "Bob" the manager of "store"
+
+  Scenario: Change store opening time (valid)
+    Given The "store" opens "Monday" at "8:00" and closes at "17:30"
+    When "Bob" changes opening time of "Monday" to "7:30"
+    Then The "store" opening of "Monday" is "7:30"
+
+  Scenario: Change store opening time (invalid)
+    Given The "store" opens "Monday" at "8:00" and closes at "17:30"
+    When "Bob" changes opening time of "Monday" to "18:30"
+    Then Changing opening time of "Monday" fails
+
+  Scenario: Change store closing time (valid)
+    Given The "store" opens "Monday" at "8:00" and closes at "17:30"
+    When "Bob" changes closing time of "Monday" to "18:30"
+    Then The "store" closing of "Monday" is "18:30"
+
+  Scenario: Change store closing time (invalid)
+    Given The "store" opens "Monday" at "8:00" and closes at "17:30"
+    When "Bob" changes closing time of "Monday" to "7:30"
+    Then Changing closing time of "Monday" fails

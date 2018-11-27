d4c366d1e78790d89f8f1858e9d4cfc93523c36c
Marion Marguerettaz <marion.marguerettaz@etu.unice.fr>
2018-11-27 08:27:49 +0100

    #43 possibilité d'ajouter/enlever des cookies d'une commande

--- src/main/java/main/Facade.java
+++ src/main/java/main/Facade.java
@@ -4,7 +4,10 @@ import order.Order;
 import order.OrderState;
 import store.Store;
 
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
+import java.time.temporal.TemporalAdjuster;
+import java.time.temporal.TemporalAdjusters;
 import java.util.ArrayList;
 import java.util.Optional;
 
@@ -12,7 +15,7 @@ public class Facade {
 
     private CookieFirm cookieFirm;
 
-    public Facade(CookieFirm cookieFirm){
+    public Facade(CookieFirm cookieFirm) {
         this.cookieFirm = cookieFirm;
     }
 
@@ -24,14 +27,14 @@ public class Facade {
 
 
     public void createACustomer(String sName, String sLastName, String phoneNumber, String email, String
-            password){
+            password) {
         this.cookieFirm.createAccount(sName, sLastName, phoneNumber, email, password);
     }
 
 
-    public boolean addACustomerToLP(String sEmail){
+    public boolean addACustomerToLP(String sEmail) {
         Optional<Customer> customer = this.cookieFirm.findCustomer(sEmail);
-        if(customer.isPresent()) {
+        if (customer.isPresent()) {
             this.cookieFirm.addCustomerToLoyaltyProgram(customer.get());
             return true;
         }
@@ -39,24 +42,24 @@ public class Facade {
     }
 
     public void customerCreateCookiesAndPlaceOrder(String sEmail, String sStore, int nbCookies, int pickupTime, String pickUpDay,
-                                     Boolean payedOnline){
+                                                   Boolean payedOnline) {
         Optional<Customer> opCustomer = this.cookieFirm.findCustomer(sEmail);
         Optional<Store> opStore = this.cookieFirm.findStore(sStore);
 
-        if(opCustomer.isPresent() && opStore.isPresent()){
-            Order order = new Order(opStore.get(), LocalDateTime.now().plusHours(pickupTime), Day.dayFromName(pickUpDay));
+        if (opCustomer.isPresent() && opStore.isPresent()) {
+            Order order = new Order(opStore.get(), generateTime(pickupTime, pickUpDay));
             order.addCookie(opStore.get().getMonthlyRecipe(), nbCookies);
             opCustomer.get().setTemporaryOrder(order);
             opCustomer.get().placeOrder(payedOnline);
         }
     }
 
-    public void customerModifyHisOrder(String sEmail, String sStore, int nbCookies, boolean remove){
+    public void customerModifyHisOrder(String sEmail, String sStore, int nbCookies, boolean remove) {
         Optional<Customer> customer = this.cookieFirm.findCustomer(sEmail);
         Optional<Store> store = this.cookieFirm.findStore(sStore);
 
-        if(customer.isPresent() && store.isPresent()){
-            if(!remove)
+        if (customer.isPresent() && store.isPresent()) {
+            if (!remove)
                 customer.get().getTemporaryOrder().addCookie(store.get().getMonthlyRecipe(), nbCookies);
             else
                 customer.get().getTemporaryOrder().removeCookie(store.get().getMonthlyRecipe(), nbCookies);
@@ -70,19 +73,17 @@ public class Facade {
     }*/
 
 
-
-    public boolean anEmployeeMakeAnActionOnOrder(String sStore, int time, String day, String email, String action){
+    public boolean anEmployeeMakeAnActionOnOrder(String sStore, int time, String day, String email, String action) {
         Optional<Store> opStore = this.cookieFirm.findStore(sStore);
 
-        if(opStore.isPresent()){
-            Optional<Order> opOrder = opStore.get().findOrder(LocalDateTime.now().plusHours(time), Day.dayFromName(day), email);
+        if (opStore.isPresent()) {
+            Optional<Order> opOrder = opStore.get().findOrder(generateTime(time, day), email);
 
-            if(opOrder.isPresent()) {
-                if(action.equals("cancel")) {
+            if (opOrder.isPresent()) {
+                if (action.equals("cancel")) {
                     opOrder.get().cancel();
                     return true;
-                }
-                else if(action.equals("withdrawn")){
+                } else if (action.equals("withdrawn")) {
                     opOrder.get().withdraw();
                     return true;
                 }
@@ -90,4 +91,10 @@ public class Facade {
         }
         return false;
     }
+
+
+    private LocalDateTime generateTime(int pickupTime, String pickUpDay) {
+        return LocalDateTime.now().plusHours(pickupTime).with(TemporalAdjusters
+                .next(DayOfWeek.valueOf(pickUpDay.toUpperCase())));
     }
+}

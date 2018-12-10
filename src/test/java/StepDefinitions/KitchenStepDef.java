0da1d3cbf91db5d69852e872ec6bed66b38fc7ae
Marion Marguerettaz <marion.marguerettaz@etu.unice.fr>
2018-12-10 16:22:53 +0100

    #52 add cookies gherkin fonctionnel

---
+++ src/test/java/StepDefinitions/KitchenStepDef.java
@@ -0,0 +1,35 @@
+package StepDefinitions;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Then;
+import recipe.ingredient.Ingredient;
+import recipe.ingredient.Topping;
+import store.Store;
+import utils.CucumberContext;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class KitchenStepDef {
+
+
+    private final CucumberContext context = CucumberContext.getContext();
+
+    @Then("^The kitchen of \"([^\"]*)\" has exactly or more than (\\d+) \"([^\"]*)\" \"([^\"]*)\"$")
+    public void theKitchenOfHasExactly(String store, int quantity, String type, String ingredient) throws Throwable {
+
+        Optional<Store> opStore = context.cookieFirm().findStore(store);
+
+        if(opStore.isPresent())
+        {
+            if(type.equals("topping"))
+                assert(opStore.get().getKitchen().hasInStock(context.getFacade().toppingFromName(ingredient),quantity));
+            else if(type.equals("dough"))
+                assert(opStore.get().getKitchen().hasInStock(context.getFacade().doughFromName(ingredient),quantity));
+            else if(type.equals("flavor"))
+                assert(opStore.get().getKitchen().hasInStock(context.getFacade().flavorFromName(ingredient),quantity));
+
+        }
+    }
+}
--- src/test/resources/features/Kitchen.feature
+++ src/test/resources/features/Kitchen.feature
@@ -1,10 +1,10 @@
 Feature: Manage the stock of a Kitchen
 
   Background:
-    Given A store "emptyStore"
+    Given A store "emptyStore" with a tax 1 and margin on recipe 1
     And "Manager" the manager of "emptyStore"
 
   Scenario: Refill the kitchen for One Ingredient
     Given The kitchen of "emptyStore" is empty
     When The manager refill the stock of "topping" "White Chocolate" by 5 in the kitchen of "emptyStore"
-    Then The kitchen of "emptyStore" has exactly 5 "topping" "White Chocolate"
+    Then The kitchen of "emptyStore" has exactly or more than 5 "topping" "White Chocolate"
